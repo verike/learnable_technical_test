c3815e59a0cf3614764f1982ff218987eb81cb7e
Victor Erike <verike7@gmail.com>
2022-09-11 21:54:04 +0100

    nth rarest: I wrapped the nth_most_rare function with the signature function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from inspect import signature
 
 
 int_list = [1,2,3,4,5,6,7,92,3,2,35,9,2,43,4,9,9,9]
@@ -6,14 +7,19 @@ int_list = [1,2,3,4,5,6,7,92,3,2,35,9,2,43,4,9,9,9]
 def nth_most_rare(list_arr, n):
     list_arr = Counter(list_arr).items()
     
-
-    for (k,v) in list_arr:
-        if n == v:
-            nth_rarest = f' {k} is the number {v} rarest in the item list'
+    # This loop below extracts the key as the item and the value as
+    #  the number of times the item occurred 
+    for (item,occurrance) in list_arr:
+        if n == occurrance:
+            nth_rarest = f' {item} is the number {n} rarest in the listed items'
             return print(nth_rarest)
         
 
     # complist = sorted([(k, v) for k,v in list_arr],reverse=True)[n-1][1]
     # return complist
 
-nth_most_rare(int_list,)
+# The signature function returns the arguments passed into the functions parameter
+sign = signature(nth_most_rare)
+
+nth_most_rare(int_list,3)
+print(sign)
