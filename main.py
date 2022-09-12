7dc7d648145c459e33d0e54aaaf01809b61d097d
Victor Erike <verike7@gmail.com>
2022-09-12 10:19:36 +0100

    nth rarest program: Made a few modifications to the code ; added a new list for tests git commit -m

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from inspect import signature
 
 
 int_list = [1,2,3,4,5,6,7,92,3,2,35,9,2,43,4,9,9,9]
+string_list = ['a','james','solomomon','sandra', 'a','a', 'victor', 'vector']
 
-def nth_most_rare(list_arr, n):
+def nth_most_rare(list_arr, n:int):
     list_arr = Counter(list_arr).items()
     
     # This loop below extracts the key as the item and the value as
@@ -18,8 +19,13 @@ def nth_most_rare(list_arr, n):
     # complist = sorted([(k, v) for k,v in list_arr],reverse=True)[n-1][1]
     # return complist
 
-# The signature function returns the arguments passed into the functions parameter
+# The signature function is assigned to a variable and
+#  returns the arguments passed into the functions parameter
 sign = signature(nth_most_rare)
 
+# calling the function
 nth_most_rare(int_list,3)
-print(sign)
+nth_most_rare(string_list,1)
+
+# reveals the properties of the function wrapped inside the signature function 
+print(f'Signature of function: {sign}')
